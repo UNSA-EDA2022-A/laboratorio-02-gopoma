99e24960d64fd9ac920396f002841073cd805870
Gustavo Ordoño Poma <88157777+gopoma@users.noreply.github.com>
2022-05-12 09:45:16 -0500

    Solved and made the exaplanation for Exercise 1

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -15,7 +15,15 @@ public class Exercise1 {
 	}
 
 	public boolean esCuadradoPerfecto(int numero) {
-
-		return false;
+		return (numero > 0)? isSquarePerfectFromAStart(numero, 0) : false; // Indirect Recursion
+    		// numero can't be lower than 0 for math's rule and not cero because of the condition
 	}
+
+  	public boolean isSquarePerfectFromAStart(int num, int start) {
+    		if(num == start * start) // base case
+      			return true;
+
+    		return (start < num)? isSquarePerfectFromAStart(num, start + 1) : false;
+    		// If start is lower than num, we continue recursing, else we finish to avoid a wrong answer
+  	}
 }
